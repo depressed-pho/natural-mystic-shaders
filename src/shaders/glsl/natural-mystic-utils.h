c85291778bf9f4b9784d5a34ebee07475c36578d
PHO <pho@cielonegro.org>
2018-12-19 00:15:31 +0900

    Add sunlight and daylight; changed the way how we perform tone mapping

--- src/shaders/glsl/natural-mystic-utils.h
+++ src/shaders/glsl/natural-mystic-utils.h
@@ -1,19 +1,28 @@
 // -*- glsl -*-
 
-/* Calculate and apply a shadow on the original fragment "frag". The
- * argument "torch" should be the torch light level [0, 1] and "sun"
- * should be the sunlight level [0, 1].
+/* Apply the ambient light on the original fragment "frag". Without
+ * this filter, objects getting no light will be rendered in complete
+ * darkness, which isn't how the reality works.
  */
-vec3 applyShadow(vec3 frag, float torch, float sun) {
+vec3 applyAmbientLight(vec3 frag) {
+    vec3 level = max(vec3(0.1), 0.5 - frag) * 1.2 + 1.0;
+    return frag * level;
+}
+
+/* Calculate and apply a shadow on the original fragment "frag". The
+ * argument "torchLevel" should be the torch light level [0, 1] and
+ * "sunLevel" should be the sunlight level [0, 1].
+ */
+vec3 applyShadow(vec3 frag, float torchLevel, float sunLevel) {
     const vec3 shadowColor = vec3(0.0);
-    const float minShadowLevel = 0.0;  // [0, 1]
-    const float maxShadowLevel = 0.45; // [0, 1]
+    const float minShadow = 0.0;  // [0, 1]
+    const float maxShadow = 0.45; // [0, 1]
     const float torchLightCutOff = 0.1; // [0, 1]
 
     /* The less the sunlight level is, the darker the fragment will
      * be. */
-    float shadowLevel = mix(maxShadowLevel, minShadowLevel,
-                            smoothstep(0.865, 0.875, sun));
+    float amount = mix(maxShadow, minShadow,
+                       smoothstep(0.865, 0.875, sunLevel));
 
     /* The existence of torch light should negate the effect of
      * shadows. The higher the torch light level is, the less the
@@ -21,27 +30,54 @@ vec3 applyShadow(vec3 frag, float torch, float sun) {
      * to completely drive away the shadow, since the intensity of the
      * torch light wouldn't be comparable to that of sunlight.
      */
-    shadowLevel *= mix(1.0, torchLightCutOff,
-                       smoothstep(0.5, 1.0, torch));
+    amount *= mix(1.0, torchLightCutOff,
+                  smoothstep(0.5, 1.0, torchLevel));
 
-    return mix(frag, shadowColor, shadowLevel);
+    return mix(frag, shadowColor, amount);
 }
 
 /* Calculate and apply the torch color on the original fragment
- * "frag". The argument "torch" should be the torch light level [0, 1]
- * and "sun" should be the sunlight level [0, 1].
+ * "frag". The argument "torchLevel" should be the torch light level [0, 1]
+ * and "sunLevel" should be the sunlight level [0, 1].
  */
-vec3 applyTorchColor(vec3 frag, float torch, float sun) {
+vec3 applyTorchColor(vec3 frag, float torchLevel, float sunLevel) {
     const vec3 torchColor = vec3(0.8, 0.3, -0.2);
-    const float torchIntensity = 0.6; // [0, 1]
+    const float torchDecay = 0.6; // [0, 1]
     const float sunlightCutOff = 0.1; // [0, 1]
 
     /* The sunlight should prevent torches from affecting the color.
      */
-    float torchLevel = max(0.0, torch - torchIntensity) *
-        mix(1.0, sunlightCutOff, smoothstep(0.7, 0.9, sun));
+    float amount = max(0.0, torchLevel - torchDecay) *
+        mix(1.0, sunlightCutOff, smoothstep(0.7, 0.9, sunLevel));
 
-    return min(frag + torchColor * torchLevel, 1.0);
+    return frag + torchColor * amount;
+}
+
+/* Apply the sunlight on a fragment "frag" based on the time-dependent
+ * daylight level "daylight" [0,1]. The argument "sunLevel" should be
+ * the terrain-dependent sunlight level [0,1]. The sunlight is
+ * yellow-ish.
+ */
+vec3 applySunlight(vec3 frag, float sunLevel, float daylight) {
+    const vec3 sunColor = vec3(0.6, 0.3, 0.0);
+
+    float amount = (0.5 - abs(0.5 - daylight)) * sunLevel;
+    float sunset = (frag.r + frag.g + frag.b) / 1.5;
+
+    return mix(frag, sunset * sunColor, amount);
+}
+
+/* Apply the daylight (or sky light) on a fragment "frag" based on the
+ * time-dependent daylight level "daylight" [0,1]. The argument
+ * "sunLevel" should be the terrain-dependent sunlight level
+ * [0,1]. The daylight is blue-ish white.
+ */
+vec3 applyDaylight(vec3 frag, float sunLevel, float daylight) {
+    const vec3 skyColor = vec3(0.7, 0.7, 1.0);
+
+    float amount = sunLevel * 0.1 * daylight;
+
+    return frag + skyColor * amount;
 }
 
 /* Apply Uncharted 2 tone mapping to the original fragment "frag".
@@ -57,16 +93,14 @@ vec3 uncharted2ToneMap_(vec3 x) {
 
     return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
 }
-vec3 uncharted2ToneMap(vec3 frag) {
+vec3 uncharted2ToneMap(vec3 frag, float exposureBias) {
     const float W = 11.2;
-    const float exposureBias = 2.0;
-    const float gamma = 1.3;
 
     vec3 curr = uncharted2ToneMap_(exposureBias * frag);
     vec3 whiteScale = 1.0 / uncharted2ToneMap_(vec3(W, W, W));
     vec3 color = curr * whiteScale;
 
-    return pow(color, vec3(1.0 / gamma));
+    return color;
 }
 
 /* Apply ACES filmic tone mapping to the original fragment "x".
@@ -83,13 +117,19 @@ vec3 acesFilmicToneMap(vec3 x) {
     return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
 }
 
-/* Apply an HDR filter to the original fragment "frag". */
-vec3 hdrFilter(vec3 frag, float contrast, float overExposure, float underExposure) {
+/* Apply a contrast filter to the original fragment "frag".
+ */
+vec3 contrastFilter(vec3 frag, float contrast) {
+    return (frag - 0.5) * max(contrast, 0.0) + 0.5;
+}
+
+/* Apply an HDR exposure and contrast filter to the original LDR
+ * fragment "frag". The resulting image will be HDR, and needs to be
+ * tone-mapped back to LDR at the last stage. */
+vec3 hdrExposure(vec3 frag, float overExposure, float underExposure) {
     vec3 overExposed   = frag / overExposure;
     vec3 normalExposed = frag;
     vec3 underExposed = frag * underExposure;
 
-    frag = mix(overExposed, underExposed, normalExposed);
-    frag = (frag - 0.5) * max(contrast, 0.0) + 0.5;
-    return frag;
+    return mix(overExposed, underExposed, normalExposed);
 }
