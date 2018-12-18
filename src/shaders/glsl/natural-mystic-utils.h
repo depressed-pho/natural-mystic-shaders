f4c5338255baa509808e4661ba749c17adf83e27
PHO <pho@cielonegro.org>
2018-12-18 10:04:40 +0900

    add HDR filter

--- src/shaders/glsl/natural-mystic-utils.h
+++ src/shaders/glsl/natural-mystic-utils.h
@@ -43,3 +43,53 @@ vec3 applyTorchColor(vec3 frag, float torch, float sun) {
 
     return min(frag + torchColor * torchLevel, 1.0);
 }
+
+/* Apply Uncharted 2 tone mapping to the original fragment "frag".
+ * See: http://filmicworlds.com/blog/filmic-tonemapping-operators/
+ */
+vec3 uncharted2ToneMap_(vec3 x) {
+    const float A = 0.15;
+    const float B = 0.50;
+    const float C = 0.10;
+    const float D = 0.20;
+    const float E = 0.02;
+    const float F = 0.30;
+
+    return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
+}
+vec3 uncharted2ToneMap(vec3 frag) {
+    const float W = 11.2;
+    const float exposureBias = 2.0;
+    const float gamma = 1.3;
+
+    vec3 curr = uncharted2ToneMap_(exposureBias * frag);
+    vec3 whiteScale = 1.0 / uncharted2ToneMap_(vec3(W, W, W));
+    vec3 color = curr * whiteScale;
+
+    return pow(color, vec3(1.0 / gamma));
+}
+
+/* Apply ACES filmic tone mapping to the original fragment "x".
+ * See:
+ * https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+ */
+vec3 acesFilmicToneMap(vec3 x) {
+    const float a = 2.51;
+    const float b = 0.03;
+    const float c = 2.43;
+    const float d = 0.59;
+    const float e = 0.14;
+
+    return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
+}
+
+/* Apply an HDR filter to the original fragment "frag". */
+vec3 hdrFilter(vec3 frag, float contrast, float overExposure, float underExposure) {
+    vec3 overExposed   = frag / overExposure;
+    vec3 normalExposed = frag;
+    vec3 underExposed = frag * underExposure;
+
+    frag = mix(overExposed, underExposed, normalExposed);
+    frag = (frag - 0.5) * max(contrast, 0.0) + 0.5;
+    return frag;
+}
