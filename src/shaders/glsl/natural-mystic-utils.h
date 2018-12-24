9e8e95f16921d27f9670784fbca18e697314b914
PHO <pho@cielonegro.org>
2018-12-24 18:26:22 +0900

    Replace clouds with our own generated one (#17)

--- src/shaders/glsl/natural-mystic-utils.h
+++ src/shaders/glsl/natural-mystic-utils.h
@@ -13,6 +13,52 @@ vec3 desaturate(vec3 color, float degree) {
     return mix(color, vec3(luma), degree);
 }
 
+/* Generate a random scalar based on some 2D vector. See
+ * https://thebookofshaders.com/13/
+ */
+float random(vec2 st) {
+    return fract(
+        sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+/* Generate a perlin noise based on some 2D vector. Based on Morgan
+ * McGuire @morgan3d https://www.shadertoy.com/view/4dS3Wd
+ */
+float perlinNoise(vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+        (c - a)* u.y * (1.0 - u.x) +
+        (d - b) * u.x * u.y;
+}
+
+/* Generate an fBM noise based on some 2D vector. See
+ * https://thebookofshaders.com/13/
+ */
+float fBM(int octaves, vec2 st) {
+    // Initial values
+    float value = 0.0;
+    float amplitude = 0.5;
+    float frequency = 0.0;
+
+    // Loop of octaves
+    for (int i = 0; i < octaves; i++) {
+        value += amplitude * perlinNoise(st);
+        st *= 2.0;
+        amplitude *= 0.5;
+    }
+    return value;
+}
+
 /* Apply the ambient light on the original fragment "frag". Without
  * this filter, objects getting no light will be rendered in complete
  * darkness, which isn't how the reality works.
