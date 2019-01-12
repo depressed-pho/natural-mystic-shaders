bb6cc61379c8b264f660cd116a4377b513f745dd
PHO <pho@cielonegro.org>
2019-01-12 10:51:52 +0900

    Use Simplex noise instead of Perlin noise to generate the cloud map

--- src/shaders/glsl/natural-mystic-noise.h
+++ src/shaders/glsl/natural-mystic-noise.h
@@ -42,12 +42,16 @@ highp float perlinNoise(highp vec2 st) {
 }
 
 /* Permutation in mod 289. */
-highp vec4 permute289(highp vec4 x) {
-    return mod(((x * 34.0) + 1.0) * x, 289.0);
+highp float permute289(highp float x) {
+    return mod((x * 34.0 + 1.0) * x, 289.0);
 }
 
-highp float permute289(highp float x) {
-    return mod(((x * 34.0) + 1.0) * x, 289.0);
+highp vec3 permute289(highp vec3 x) {
+    return mod((x * 34.0 + 1.0) * x, 289.0);
+}
+
+highp vec4 permute289(highp vec4 x) {
+    return mod((x * 34.0 + 1.0) * x, 289.0);
 }
 
 /* A 4D gradient function, used for generating noise. */
@@ -60,6 +64,55 @@ highp vec4 grad4(highp float j, highp vec4 ip) {
     p.xyz -= sign(p.xyz) * (p.w < 0.0 ? 1.0 : 0.0);
 
     return p;
+}
+
+/* 2D simplex noise [-1, 1], based on https://github.com/stegu/webgl-noise/
+ */
+highp float simplexNoise(highp vec2 v) {
+    const highp vec4 C = vec4(
+        0.211324865405187,   // (3.0-sqrt(3.0))/6.0
+        0.366025403784439,   // 0.5*(sqrt(3.0)-1.0)
+        -0.577350269189626,  // -1.0 + 2.0 * C.x
+        0.024390243902439);  // 1.0 / 41.0
+
+    // First corner
+    highp vec2 i  = floor(v + dot(v, C.yy));
+    highp vec2 x0 = v -   i + dot(i, C.xx);
+
+    // Other corners
+    highp vec2 i1  = x0.x > x0.y ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+    highp vec4 x12 = x0.xyxy + C.xxzz;
+    x12.xy -= i1;
+
+    // Permutations
+    i = mod(i, 289.0); // Avoid truncation effects in permutation
+    highp vec3 p =
+        permute289(
+            permute289(
+                i.y + vec3(0.0, i1.y, 1.0)
+                ) + i.x + vec3(0.0, i1.x, 1.0)
+            );
+
+    highp vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), 0.0);
+    m = m*m;
+    m = m*m;
+
+    // Gradients: 41 points uniformly over a line, mapped onto a
+    // diamond.  The ring size 17*17 = 289 is close to a multiple of
+    // 41 (41*7 = 287)
+    highp vec3 x  = 2.0 * fract(p * C.www) - 1.0;
+    highp vec3 h  = abs(x) - 0.5;
+    highp vec3 ox = round(x);
+    highp vec3 a0 = x - ox;
+
+    // Normalise gradients implicitly by scaling m
+    m *= inversesqrt(a0 * a0 + h * h);
+
+    // Compute final noise value at P
+    highp vec3 g;
+    g.x  = a0.x  * x0.x   + h.x  * x0.y;
+    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+    return 130.0 * dot(m, g);
 }
 
 /* 4D simplex noise [-1, 1], based on https://github.com/stegu/webgl-noise/
@@ -160,7 +213,7 @@ highp float fBM(int octaves, highp vec2 st) {
 
     // Loop of octaves
     for (int i = 0; i < octaves; i++) {
-        value += amplitude * perlinNoise(st);
+        value += amplitude * (simplexNoise(st) * 0.5 + 0.5);
         st *= 2.0;
         amplitude *= 0.5;
     }
