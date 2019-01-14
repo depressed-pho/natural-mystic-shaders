68700f621d41bc9086c89f2f4eb10a2ef5e62d64
PHO <pho@cielonegro.org>
2019-01-14 10:56:27 +0900

    Use the real normal to calculate the opacity of water

--- src/shaders/glsl/natural-mystic-water.h
+++ src/shaders/glsl/natural-mystic-water.h
@@ -18,7 +18,7 @@
  */
 
 highp vec3 gerstnerWave(
-    highp vec3 wPos, highp float time, out highp vec3 normal,
+    highp vec3 wPos, highp float time, inout highp vec3 normal,
     float Q, float numWaves, float Ai, vec2 Di, float Li, float Si) {
 
     const float wFactor = 9.80665 * 2.0 * 3.14159;
@@ -30,6 +30,10 @@ highp vec3 gerstnerWave(
     highp float phase = wi * dot(Di, wPos.xz) + phi_i * time;
     wPos.xz += Qi * Ai * Di * cos(phase);
     wPos.y  +=      Ai      * sin(phase);
+
+    highp float wiAi = wi * Ai;
+    normal.xz -= wiAi * Di * cos(phase);
+    normal.y  -= wiAi * Qi * sin(phase);
 
     return wPos;
 }
@@ -72,16 +76,35 @@ highp vec3 geometricWaterWave(highp vec3 wPos, highp float time, out highp vec3 
      * N(x, y, t) = |    -Σ(D_i.y * w_i A_i * cos(w_i D_i · (x, y) + φ_i t)), |
      *              [ 1 - Σ(Q_i   * w_i A_i * sin(w_i D_i · (x, y) + φ_i t))  ]
      */
-    const float Q        = 0.4;
+    const float Q        = 0.45;
     const float numWaves = float(4);
 
     normal = vec3(0.0, 1.0, 0.0);
-    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.07, deg2dir( 90.0), 16.0, 6.0);
-    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.07, deg2dir(260.0), 15.0, 6.0);
-    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.03, deg2dir( 50.0),  8.0, 8.0);
-    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.03, deg2dir(200.0),  7.0, 8.0);
+    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.08, deg2dir( 90.0), 16.0, 7.0);
+    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.08, deg2dir(260.0), 15.0, 8.0);
+    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.05, deg2dir( 70.0),  8.0, 13.0);
+    wPos   = gerstnerWave(wPos, time, normal, Q, numWaves, 0.02, deg2dir(200.0),  7.0, 14.0);
 
+    normal = normalize(normal);
     return wPos;
+}
+
+vec4 waterColor(vec4 baseColor, vec3 ambient, highp vec3 relPos, highp vec3 normal) {
+    /* We want to know the angle between the normal of the water plane
+     * (or anything rendered similarly to water) and the camera. */
+    highp float cosTheta = abs(dot(normal, normalize(-relPos))); // [0, 1]
+
+    /* Use the angle to calculate the color of surface. The more steep
+     * the angle is, the less intensity of the supecular light will
+     * be. And we also use the angle to determine the opacity, that
+     * is, when the angle is wide the water plane behaves more like a
+     * mirror than air.
+     */
+    float opacity  = baseColor.a;
+    vec4 surfColor = vec4(baseColor.rgb, min(1.0, baseColor.a * 8.0));
+    vec4 nearColor = vec4(baseColor.rgb, opacity);
+
+    return mix(surfColor, nearColor, cosTheta);
 }
 
 #endif /* !defined(NATURAL_MYSTIC_WATER_H_INCLUDED) */
