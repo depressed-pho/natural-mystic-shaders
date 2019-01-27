7b50a9526db49585e488cc318096cfb28f01e5bc
PHO <pho@cielonegro.org>
2019-01-27 10:12:08 +0900

    Optimization: Don't compute water ripples on far terrain

--- src/shaders/glsl/natural-mystic-rain.h
+++ src/shaders/glsl/natural-mystic-rain.h
@@ -18,18 +18,32 @@ float wetness(float clearWeather, float sunLevel) {
     return (1.0 - clearWeather) * smoothstep(shadowBorder - shadowBlur, shadowBorder + shadowBlur, sunLevel);
 }
 
-vec3 ripples(vec3 incomingLight, highp vec3 worldPos, highp float time, highp vec3 normal) {
-    /* Water ripples should only be apparent on the ground, i.e. where the
-     * normal matches to (0, 1, 0). */
-    float cosTheta = max(0.0, normal.y); // Equivalent to max(0.0, dot(normal, vec3(0, 1, 0)));
+/* Compute light reflected by water ripples on the ground.
+ */
+vec3 ripples(vec3 incomingLight, highp vec3 worldPos, highp float cameraDist, highp float time, highp vec3 normal) {
+    /* The visual effect of ripples is so subtle, and it won't be
+     * visible on far terrain. We can skip the costly noise generation
+     * unless worldPos isn't close to the camera. */
+    const float distThreshold = 0.05;
+    const float distFadeStart = distThreshold * 0.8;
 
-    const highp vec3 resolution = vec3(vec2(0.15), 0.5);
-    const float amount = 0.1;
+    if (cameraDist < distThreshold) {
+        /* Water ripples should only be apparent on the ground, i.e. where the
+         * normal matches to (0, 1, 0). */
+        float cosTheta = max(0.0, normal.y); // Equivalent to max(0.0, dot(normal, vec3(0, 1, 0)));
 
-    highp vec3 st = vec3(worldPos.xz, time) / resolution;
-    float ripples = (simplexNoise(st) + 0.8) * 0.5;
+        const highp vec3 resolution = vec3(vec2(0.15), 0.5);
+        const float amount = 0.1;
 
-    return mix(0.2, 1.0, cosTheta) * incomingLight * ripples * amount;
+        highp vec3 st = vec3(worldPos.xz, time) / resolution;
+        float ripples = (simplexNoise(st) + 0.8) * 0.5;
+
+        return mix(0.2, 1.0, cosTheta) * incomingLight * ripples * amount *
+            (1.0 - smoothstep(distFadeStart, distThreshold, cameraDist));
+    }
+    else {
+        return vec3(0);
+    }
 }
 
 #endif /* defined(NATURAL_MYSTIC_RAIN_H_INCLUDED) */
